d6c09a264b1566ad73f98d148fa160db6be6935a
Corey Vixie <corey@vixie.enterprises>
2021-09-18 19:33:46 -0700

    Add storybook boilerplate

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,10 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  "core": {
-    "builder": "webpack5"
-  }
-}
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  core: {
+    builder: "webpack5",
+  },
+};
