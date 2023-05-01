51196948e6ac4f59e06260004263a8599677e509
Corey Vixie <>
2023-05-01 15:55:45 -0700

    Upgrade to modern Next

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
   eslint: {
     dirs: [".storybook", "pages", "components", "stories", "styles"],
   },
