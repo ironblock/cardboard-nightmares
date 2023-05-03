4ad64b74d41c94d155ba6e9deba8df1b9439240c
Corey Vixie <>
2023-05-02 17:50:17 -0700

    More accurate ImageURI types

--- types/Scryfall/Attributes.ts
+++ types/Scryfall/Attributes.ts
@@ -6,8 +6,11 @@ export type Colors = Color[];
 export type GameContext = "paper" | "arena" | "mtgo";
 export type Games = GameContext[];
 
-export interface ImageURIs {
-  small: URI;
-  normal: URI;
-  large: URI;
+export interface ImageURIs<I extends UUID> {
+  art_crop: `"https://cards.scryfall.io/art_crop/front/0/b/${I}.jpg?${number}`;
+  border_crop: `"https://cards.scryfall.io/border_crop/front/0/b/${I}.jpg?${number}`;
+  large: `"https://cards.scryfall.io/large/front/0/b/${I}.jpg?${number}`;
+  normal: `"https://cards.scryfall.io/normal/front/0/b/${I}.jpg?${number}`;
+  png: `"https://cards.scryfall.io/png/front/0/b/${I}.png?${number}`;
+  small: `"https://cards.scryfall.io/small/front/0/b/${I}.jpg?${number}`;
 }
